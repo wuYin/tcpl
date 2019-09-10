311174335b79ccd7ba5afa4ac37af40e802a72a4
wuYin <wuYinPost@gmail.com>
2019-09-10 10:43:59 +0800

    第二章：类型、运算符与表达式

--- ch2/2.2.c
+++ ch2/2.2.c
@@ -7,35 +7,32 @@
 
 int getOneLine(char s[], int lim);
 
-main()
-{
-    char line[MAX_LINE];
-    int n;
-    if (getOneLine(line, MAX_LINE) > 0)
-        printf("%s", line);
+main() {
+	char line[MAX_LINE];
+	int n;
+	if (getOneLine(line, MAX_LINE) > 0)
+		printf("%s", line);
 }
 
-enum loop
-{
-    NO,
-    YES
+enum loop {
+	NO, // 0
+	YES // 1
 };
 
 enum loop ok = YES; // 枚举变量前缀也需 enum 修饰符，和 struct 一致
 
-int getOneLine(char s[], int lim)
-{
-    int i = 0;
-    int c;
-    while (ok)
-        if (i >= lim - 1)
-            ok = NO;
-        else if ((c = getchar()) == EOF)
-            ok = NO;
-        else if (c == '\n')
-            ok = NO;
-        else
-            s[i++] = c;
+int getOneLine(char s[], int lim) {
+	int i = 0;
+	int c;
+	while (ok)
+		if (i >= lim - 1)
+			ok = NO;
+		else if ((c = getchar()) == EOF)
+			ok = NO;
+		else if (c == '\n')
+			ok = NO;
+		else
+			s[i++] = c;
 
-    return i;
+	return i;
 }
--- ch2/demo11.c
+++ ch2/demo11.c
@@ -4,7 +4,12 @@
 #define MAX(a, b) (a > b) ? (a) : (b)
 
 // 2.11 条件表达式，即三元运算符
-main()
-{
-    printf("%d\n", MAX(1, 2)); // 2
+main() {
+	printf("%d\n", MAX(1, 2)); // 2
+
+	// 每行打印 3 个元素
+	int nums[7] = {1, 2, 3, 5, 6, 7};
+	for (int i = 0; i < 7; ++i) {
+		printf("%3d %c", nums[i], (i % 3 == 2 || i == 7 - 1) ? '\n' : ' ');
+	}
 }
